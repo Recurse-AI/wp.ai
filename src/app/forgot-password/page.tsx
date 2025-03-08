5a992f181e64127cc4b0f25a974f78929a772c44
Unayes09 <unayeskhan.0808@gmail.com>
2025-03-08 16:36:52 +0600

    verify email, forgot pass api integrate, delete account updated

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -40,7 +40,7 @@ export default function ForgotPassword() {
     setMessage("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_AUTH_API_URL}/forgot-password/`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_AUTH_API_URL}/api/users/reset-password/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
