b9c6e0a1865396b28e75d9377e25beb0b5b06634
unknown <a.h.joy066@gmail.com>
2025-03-10 11:22:29 +0600

    feat:fix-bug-enhance-ui-and-functionality

---
+++ src/components/ToasterProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useTheme } from "@/context/ThemeProvider";
+import { getToastStyle } from "@/lib/toastConfig";
+import { Toaster } from "react-hot-toast";
+
+export default function ToasterProvider() {
+  const { theme } = useTheme();
+  
+  return (
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      toastOptions={getToastStyle(theme)}
+    />
+  );
+} 
